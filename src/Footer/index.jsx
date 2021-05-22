85b725328b8abf8a0446ab0a5bf427a26b641980
janajans <1janajansova@gmail.com>
2021-05-22 10:24:15 +0200

    2. hlavní stránka

---
+++ src/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const Footer = () => (
+  <footer>
+    <div className="container">
+      <div className="footer-title">LeviExpress</div>
+      <div className="copyright">Czechitas Digitální akademie: Web, © 2021</div>
+    </div>
+  </footer>
+);
+
+export default Footer;
---
+++ src/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => (
+  <header>
+    <div className="container">
+      <h1 className="site-title">
+        <a href="/">LeviExpress</a>
+      </h1>
+    </div>
+  </header>
+);
+
+export default Header;
