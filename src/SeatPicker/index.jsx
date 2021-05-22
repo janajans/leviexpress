1a84aa082a8a5e51b5be9881783d425df2d9c420
janajans <1janajansova@gmail.com>
2021-05-22 21:50:37 +0200

    10. Potvrzení rezervace

--- src/SeatPicker/index.jsx
+++ src/SeatPicker/index.jsx
@@ -255,10 +255,198 @@ import './style.css';
 // export default SeatPicker;
 
 // 9.17 Nyní v jednotlivých řadách zobrazíme sedadla. V datech získaných ze seats je v každé položce představující řadu sedadel další pole, prvky v něm představují jednotlivá sedadla. Každé sedadlo je reprezentováno objektem, ve kterém je číslo sedadla a údaj, zda už je obsazené. Uvnitř divu představujícího řadu sedadel tedy pomocí další funkce map budete procházet jednu řadu sedadel. Pro zobrazení sedadla použijete komponentu Seat. Můžete se inspirovat testovacími sedadly, pokud jste si je v kódu nechaly. Bude potřeba nastavit properties number, isOccupied (ty získáte z údajů o sedadle), isSelected (zjistíte, zda číslo sedadla z údajů o sedadle je shodné s číslem ve stavu selectedSeatNumber) a onSelected necháte stejně, jako to bylo u testovacích sedadel (zavoláte funkci handleSeatSelect). Když kód otestujete v prohlížeči, bude React protestovat, že chybí autribut key – ten můžete nastavit také na číslo sedadla. Vyzkoušejte v prohlížeči, že se po vyhledání spoje správně zobrazuje plánek sedadel, zobrazují se volná a obsazená sedadla, kliknutím můžete volné sedadlo vybrat.
+// const SeatPicker = ({ seats, journeyId }) => {
+//   const [selectedSeatNumber, setSelectedSeatNumber] = useState(null);
+//   const handleSeatSelect = (number) => {
+//     setSelectedSeatNumber(number);
+//   };
+//   return (
+//     <div className="seat-picker container">
+//       <h2>Vyberte sedadlo</h2>
+//       <div className="seats">
+//         {seats.map((item, index) => (
+//           <div key={index} className="seat-row">
+//             {item.map((seat) => (
+//               <Seat
+//                 number={seat.number}
+//                 isOccupied={seat.isOccupied}
+//                 isSelected={seat.number === selectedSeatNumber}
+//                 onSelect={handleSeatSelect}
+//                 key={seat.number}
+//               />
+//             ))}
+//           </div>
+//         ))}
+//       </div>
+//       <button className="btn" type="button">
+//         Rezervovat
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default SeatPicker;
+
+// 10.1 Nyní je potřeba zprovoznit tlačítko „Rezervovat". Stále v komponentě SeatPicker vytvořte funkci handleBuy a zařiďte, aby byla tato funkce volána při kliknutí na tlačítko „Rezervovat". Ve funkci si zatím můžete vypsat nějakou zprávu do konzole ('Funguju!').
+// const SeatPicker = ({ seats, journeyId }) => {
+//   const [selectedSeatNumber, setSelectedSeatNumber] = useState(null);
+//   const handleSeatSelect = (number) => {
+//     setSelectedSeatNumber(number);
+//   };
+//   const handleBuy = () => {
+//     console.log('rezervuju?');
+//   };
+//   return (
+//     <div className="seat-picker container">
+//       <h2>Vyberte sedadlo</h2>
+//       <div className="seats">
+//         {seats.map((item, index) => (
+//           <div key={index} className="seat-row">
+//             {item.map((seat) => (
+//               <Seat
+//                 number={seat.number}
+//                 isOccupied={seat.isOccupied}
+//                 isSelected={seat.number === selectedSeatNumber}
+//                 onSelect={handleSeatSelect}
+//                 key={seat.number}
+//               />
+//             ))}
+//           </div>
+//         ))}
+//       </div>
+//       <button className="btn" type="button" onClick={handleBuy}>
+//         Rezervovat
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default SeatPicker;
+
+// 10.2 Pomocí atributu disabled na tlačítku „Rezervovat" zabezpečte, aby bylo tlačítko přístupné jenom tehdy, když je vybrané nějaké sedadlo, tj. když stav selectedSeatNumber nemá hodnotu null. Ověřte v prohlížeči, že se tlačítko zpřístupní až po výběru sedadla a že se po kliknutí na tlačítko vypíše do konzole text, který máte ve funkci handleBuy.
+
+// const SeatPicker = ({ seats, journeyId }) => {
+//   const [selectedSeatNumber, setSelectedSeatNumber] = useState(null);
+//   const handleSeatSelect = (number) => {
+//     setSelectedSeatNumber(number);
+//   };
+//   const handleBuy = () => {
+//     console.log('rezervuju?');
+//   };
+//   return (
+//     <div className="seat-picker container">
+//       <h2>Vyberte sedadlo</h2>
+//       <div className="seats">
+//         {seats.map((item, index) => (
+//           <div key={index} className="seat-row">
+//             {item.map((seat) => (
+//               <Seat
+//                 number={seat.number}
+//                 isOccupied={seat.isOccupied}
+//                 isSelected={seat.number === selectedSeatNumber}
+//                 onSelect={handleSeatSelect}
+//                 key={seat.number}
+//               />
+//             ))}
+//           </div>
+//         ))}
+//       </div>
+//       <button
+//         className="btn"
+//         type="button"
+//         onClick={handleBuy}
+//         disabled={selectedSeatNumber === null}
+//       >
+//         Rezervovat
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default SeatPicker;
+
+// 10.3 Podle dokumentace React Routeru se podívejte, jak lze pomocí hooku useHistory přímo v kódu změnit stránka, na které se uživatel nachází. Tedy jak uživatele přesměrovat.
+// 10.4 Nad funkcí handleBuy vytvořte proměnnou history s použitím hooku useHistory(), nezapomeňte hook naimportovat z react-router-dom. Za moment tuto proměnnou použijeme.
+// 10.5 Nákup jízdenky se ve funkci handleBuy provede tak, že zavoláte API endpoint: https:/leviexpress-backend.herokuapp.com/api/reserve?seat=…&journeyId=…
+// Parametr seat bude číslo sedadla vybrané uživatelem a parametr journeyId bude mít hodnotu journeyId z props. Pozor, tento endpoint je nutné volat HTTP metodou POST – tělo požadavku zůstane prázdné, tj. ve fetch vůbec nebude property body.
+// 10.6 Volání tohoto API vrací JSON s daty, ze kterých nás bude zajímat hodnota reservationId. Vypište si ji do konzole.
+// import { useHistory } from 'react-router-dom';
+
+// const SeatPicker = ({ seats, journeyId }) => {
+//   const [selectedSeatNumber, setSelectedSeatNumber] = useState(null);
+//   const handleSeatSelect = (number) => {
+//     setSelectedSeatNumber(number);
+//   };
+
+//   const history = useHistory();
+//   const handleBuy = () => {
+//     fetch(
+//       `https:/leviexpress-backend.herokuapp.com/api/reserve?seat=${selectedSeatNumber}&journeyId=${journeyId}`,
+//       {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       },
+//     )
+//       .then((resp) => resp.json())
+//       .then((json) => console.log(json.data.reservationId));
+//   };
+//   return (
+//     <div className="seat-picker container">
+//       <h2>Vyberte sedadlo</h2>
+//       <div className="seats">
+//         {seats.map((item, index) => (
+//           <div key={index} className="seat-row">
+//             {item.map((seat) => (
+//               <Seat
+//                 number={seat.number}
+//                 isOccupied={seat.isOccupied}
+//                 isSelected={seat.number === selectedSeatNumber}
+//                 onSelect={handleSeatSelect}
+//                 key={seat.number}
+//               />
+//             ))}
+//           </div>
+//         ))}
+//       </div>
+//       <button
+//         className="btn"
+//         type="button"
+//         onClick={handleBuy}
+//         disabled={selectedSeatNumber === null}
+//       >
+//         Rezervovat
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default SeatPicker;
+
+// 10.7 Pomocí naší history proměnné, její metody push a hodnoty reservationId přesměrujte uživatele na stránku detailu rezervace. Takové volání bude může vypadat následovně: history.push(`/reservation/${reservationId}`);
+// 10.8 Zkontrolujte, že po vybrání sedadla a kliknutí na tlačíko „Rezervovat" se stránka přesměruje například na adresu http://localhost:8080/reservation/FwQBAAMcgLs kde záhy vytvoříme detail jízdenky!
+import { useHistory } from 'react-router-dom';
+
 const SeatPicker = ({ seats, journeyId }) => {
   const [selectedSeatNumber, setSelectedSeatNumber] = useState(null);
   const handleSeatSelect = (number) => {
     setSelectedSeatNumber(number);
+  };
+
+  const history = useHistory();
+  const handleBuy = () => {
+    fetch(
+      `https:/leviexpress-backend.herokuapp.com/api/reserve?seat=${selectedSeatNumber}&journeyId=${journeyId}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    )
+      .then((resp) => resp.json())
+      .then((json) => history.push(`/reservation/${json.data.reservationId}`));
   };
   return (
     <div className="seat-picker container">
@@ -278,7 +466,12 @@ const SeatPicker = ({ seats, journeyId }) => {
           </div>
         ))}
       </div>
-      <button className="btn" type="button">
+      <button
+        className="btn"
+        type="button"
+        onClick={handleBuy}
+        disabled={selectedSeatNumber === null}
+      >
         Rezervovat
       </button>
     </div>
