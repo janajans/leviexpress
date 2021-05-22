025703c1f5374ab9927dc7afd7a0677507963359
janajans <1janajansova@gmail.com>
2021-05-22 14:34:14 +0200

    7. Vyhledání spoje

--- src/Home/JourneyPicker/index.jsx
+++ src/Home/JourneyPicker/index.jsx
@@ -22,7 +22,23 @@ const DatesOptions = ({ dates }) => (
   </>
 );
 
-const JourneyPicker = () => {
+// Při kliknutí na tlačítko „Vyhledat spoj“ se volá funkce handleSubmit, která vypíše údaje zadané uživatelem. Nyní výpis do konzole nahradíte voláním API. Bude se volat následující API endpoint
+// https://leviexpress-backend.herokuapp.com/api/journey?fromCity=…&toCity=…&date…
+// Vytečkovaná místa se nahradí hodnotami vybranými uživatelem.
+// Volání tohoto API vrací JSON s nalezenými spoji. Vypište si výstup do konzole prohlížeče.
+
+// Nalezená spojení budeme potřebovat zobrazit v další komponentě na stránce. Potřebujeme je tedy poslat z komponenty JourneyPicker jejímu rodiči. V komponentě JourneyPicker bude property onJourneyChange, do které rodič vloží funkci, která se zavolá s údaji nalezeném spoji. Upravte hlavičku funkce JourneyPicker tak, aby bylo vidět, že akceptuje property onJourneyChange.
+
+// Ve funkci handleSubmit máte nyní výpis nalezených spojení do konzole prohlížeče. Tento výpis nahraďte voláním funkce uložené v property onJourneyChange, které jako paramter předáte data získaná z volání API. Pozor na to, že z vráceného výsledku se použije jenom jeho část – to, co je uložené pod klíčem data.
+
+//Vraťte se do komponenty Home, ze které se volá komponenta JourneyPicker. V komponentě Home vytvořte pomocí useState nový stav journey, výchozí hodnota bude null.
+
+// Propojte komponentu JourneyPicker se stavem journey – když komponenta JourneyPicker zavolá onJourneyChange s údaji o nalezeném spoji, nastaví se toto spojení do stavu journey.
+// Upravte komponentu Home tak, aby v případě, kdy ve stavu journey je nějaké spojení, vypsala pod vyhledávací formulář text „Nalezeno spojení s id …“. Místo tří teček bude id z dat o nalezeném spojení.
+
+// Ověřte, že funguje výběr měst a data, že po zadání všech třech údajů můžete kliknout na „Vyhledat spoj“ a že se po kliknutí vyhledá nějaké spojení a vypíše se do stránky jeho id.
+
+const JourneyPicker = ({ onJourneyChange }) => {
   const [fromCity, setFromCity] = useState('');
   const [toCity, setToCity] = useState('');
   const [date, setDate] = useState('');
@@ -34,8 +50,13 @@ const JourneyPicker = () => {
     console.log(fromCity);
     console.log(toCity);
     console.log(date);
+    fetch(
+      `https://leviexpress-backend.herokuapp.com/api/journey?fromCity=${fromCity}&toCity=${toCity}&date=${date}`,
+    )
+      .then((resp) => resp.json())
+      .then((json) => onJourneyChange(json.data));
   };
-
+  // onJourneyChange(json.data)
   useEffect(() => {
     fetch('https://leviexpress-backend.herokuapp.com/api/cities')
       .then((resp) => resp.json())
@@ -75,7 +96,14 @@ const JourneyPicker = () => {
             </select>
           </label>
           <div className="journey-picker__controls">
-            <button onClick={handleSubmit} className="btn" type="submit">
+            <button
+              onClick={handleSubmit}
+              className="btn"
+              type="submit"
+              disabled={
+                date === '' || toCity === '' || fromCity === '' ? true : false
+              }
+            >
               Vyhledat spoj
             </button>
           </div>
--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import JourneyPicker from './JourneyPicker/index';
 
-const Home = () => (
-  <>
-    <JourneyPicker />
-  </>
-);
+//Vraťte se do komponenty Home, ze které se volá komponenta JourneyPicker. V komponentě Home vytvořte pomocí useState nový stav journey, výchozí hodnota bude null.
+
+const Home = () => {
+  const [journey, setJourney] = useState(null);
+  const handleJourneyChange = (props) => {
+    setJourney(props);
+  };
+  return (
+    <>
+      {console.log(journey)}
+      <JourneyPicker onJourneyChange={handleJourneyChange} />
+      {journey ? <p>Nalezeno spojení s id {journey.journeyId}</p> : null}
+      {/* {journey && <p>Nalezeno spojení s id {journey.journeyId}</p>} */}
+    </>
+  );
+};
 export default Home;
+
+// Propojte komponentu JourneyPicker se stavem journey – když komponenta JourneyPicker zavolá onJourneyChange s údaji o nalezeném spoji, nastaví se toto spojení do stavu journey.
+
+// Upravte komponentu Home tak, aby v případě, kdy ve stavu journey je nějaké spojení, vypsala pod vyhledávací formulář text „Nalezeno spojení s id …“. Místo tří teček bude id z dat o nalezeném spojení.
+
+// Ověřte, že funguje výběr měst a data, že po zadání všech třech údajů můžete kliknout na „Vyhledat spoj“ a že se po kliknutí vyhledá nějaké spojení a vypíše se do stránky jeho id.
